cf0a9e6babd28bb556fbf0d9cfc47dd4723aa448
ConnerEls <elsconner02@gmail.com>
2024-07-02 14:03:27 +0200

    Bento Grid Tech Stack done, update title text to lg:text-2xl

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import BackToTopButton from "@/components/ui/BackToTopButton";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
 
@@ -8,6 +9,7 @@ export default function Home() {
 		<main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
 			<div id="top" className="max-w-7xl w-full">
 				<FloatingNav navItems={navItems} />
+				<BackToTopButton/>
 				<Hero />
 				<Grid />
 			</div>
--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/utils/cn";
 import Image from "next/image";
 import { BackgroundGradientAnimation } from "./BackgroundGradient";
 import { GlobeDemo } from "./GridGlobe";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 export const BentoGrid = ({
 	className,
@@ -44,7 +45,7 @@ export const BentoGridItem = ({
 	return (
 		<div
 			className={cn(
-				"row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border-transparent justify-between flex flex-col space-y-4 border",
+				"row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.2]",
 				className
 			)}
 		>
@@ -67,6 +68,7 @@ export const BentoGridItem = ({
 						/>
 					)}
 				</div>
+				{/* Gradient BG */}
 				{id === 6 && (
 					<BackgroundGradientAnimation className="">
 						<div className="absolute z-50 flex items-center justify-center text-white font-bold " />
@@ -82,11 +84,40 @@ export const BentoGridItem = ({
 					<div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
 						{description}
 					</div>
-					<div className="font-sans font-bold text-lg lg:text-1xl max-w-96 z-10">
+					<div className="font-sans font-bold text-lg lg:text-2xl max-w-96 z-10">
 						{title}
 					</div>
 
-					{id ===2 && <GlobeDemo/>}
+					{/* Globe */}
+					{id === 2 && <GlobeDemo/>}
+
+					{/* Tech Stack */}
+					{id === 3 && (
+						<div className="flex gap-1 lg:gap-5 w-fit absolute -right-20 lg:-right-10 opacity-85">
+							<div className="flex flex-col gap-3 lg:gap-3">
+								{['UnityVR', 'C#', 'React.js'].map((item) => (
+									<span key={item} className="py-2 lg:py-3 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-55 rounded-lg text-center bg-[#5352536e]">
+										{item}
+									</span>
+								))}
+								<span className="py-4 px-3 rounded-lg text-center bg-[#68568c71]"/>
+							</div>
+							<div className="flex flex-col gap-3 lg:gap-3">
+								<span className="py-4 px-3 rounded-lg text-center bg-[#68568c71]"/>
+								{['Next.js', 'Three.js', 'Ui/Ux'].map((item) => (
+									<span key={item} className="py-2 lg:py-3 lg:px-3 px-3 text-xs lg:text-base opacity-60 lg:opacity-70 rounded-lg text-center bg-[#5352536e]">
+										{item}
+									</span>
+								))}					
+							</div>
+						</div>
+					)}
+
+					{/* Copy Email */}
+					{id === 6 && (
+						<div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
